e27bd3d8df1e9ac0083831eddc1405c22c953a3b
rajdeep712 <rajdeeppa53@gmail.com>
2026-07-19 00:06:51 +0530

    Changed the MlFlow access credential code .Added the Flask app too. CICD Run 1

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -18,26 +18,30 @@ warnings.filterwarnings("ignore")
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = os.getenv("DAGSHUB_REPO_OWNER")
+repo_name = os.getenv("DAGSHUB_REPO_NAME")
 
 # # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
+mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
 # -------------------------------------------------------------------------------------
 
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
-dagshub.init(
-    repo_owner=os.getenv("DAGSHUB_REPO_OWNER"),
-    repo_name=os.getenv("DAGSHUB_REPO_NAME"),
-    mlflow=True,
-)
+# mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+# dagshub.init(
+#     repo_owner=os.getenv("DAGSHUB_REPO_OWNER"),
+#     repo_name=os.getenv("DAGSHUB_REPO_NAME"),
+#     mlflow=True,
+# )
 # -------------------------------------------------------------------------------------
 
 
